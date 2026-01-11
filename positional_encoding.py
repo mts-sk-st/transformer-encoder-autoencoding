605dad3d62e5665e4c259b6dd65f1ad2c316aaa5
Vijay D <vijay181723@gmail.com>
2026-01-11 11:53:12 +0530

    Initial Commit

---
+++ positional_encoding.py
@@ -0,0 +1,20 @@
+import math
+import torch
+import torch.nn as nn
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len=128):
+        super().__init__()
+        pe = torch.zeros(max_len, d_model)  
+        pos = torch.arange(0, max_len).unsqueeze(1).float()
+        div = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+
+        pe[:, 0::2] = torch.sin(pos * div)
+        pe[:, 1::2] = torch.cos(pos * div)
+
+        self.register_buffer("pe", pe.unsqueeze(0))  
+
+    def forward(self, x):
+        
+        T = x.size(1)
+        return x + self.pe[:, :T, :]
